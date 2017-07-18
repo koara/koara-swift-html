86049ff57ef09b41b9f85a4090badc156c9a278c
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2017-07-18 05:30:13 +0200

    initial setup

---
+++ KoaraHtmlTests/KoaraHtmlTests.swift
@@ -0,0 +1,19 @@
+//
+//  KoaraHtmlTests.swift
+//  KoaraHtmlTests
+//
+//  Created by Andy Van Den Heuvel on 18/07/2017.
+//  Copyright © 2017 Andy Van Den Heuvel. All rights reserved.
+//
+
+import XCTest
+
+class KoaraHtmlTests: XCTestCase {
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+workspace 'Koara'
+
+target 'KoaraHtml iOS' do
+  use_frameworks!
+  pod 'Koara', '~> 0.13'
+
+  target 'KoaraHtml iOS Tests' do
+    pod 'Quick'
+    pod 'Nimble'
+  end
+end
