8d3ce9c3b00fdf6ad1a1f86957d9f96e140668b0
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2017-07-24 05:34:26 +0200

    Reintstall pods

---
+++ Source/KoaraHtml.h
@@ -0,0 +1,19 @@
+//
+//  KoaraHtml iOS.h
+//  KoaraHtml iOS
+//
+//  Created by Andy Van Den Heuvel on 24/07/2017.
+//  Copyright © 2017 Andy Van Den Heuvel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KoaraHtml iOS.
+FOUNDATION_EXPORT double KoaraHtml_iOSVersionNumber;
+
+//! Project version string for KoaraHtml iOS.
+FOUNDATION_EXPORT const unsigned char KoaraHtml_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KoaraHtml_iOS/PublicHeader.h>
+
+
