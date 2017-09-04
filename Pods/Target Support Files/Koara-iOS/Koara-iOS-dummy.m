a4c35d8f98719463bcc698a69a3e00e09e83c0f5
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2017-09-04 19:35:37 +0200

    Add pods directly

---
+++ Pods/Target Support Files/Koara-iOS/Koara-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Koara_iOS : NSObject
+@end
+@implementation PodsDummy_Koara_iOS
+@end
---
+++ Pods/Target Support Files/Koara-tvOS/Koara-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Koara_tvOS : NSObject
+@end
+@implementation PodsDummy_Koara_tvOS
+@end
