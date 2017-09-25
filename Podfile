64bc3f50b2b895a4918be69c3806183a06d8311e
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2017-09-25 05:59:50 +0200

    Add Podfile

---
+++ Podfile
@@ -0,0 +1,12 @@
+workspace 'KoaraHtml'
+
+target 'KoaraHtml iOS' do
+  use_frameworks!
+  platform :ios, '10.3'
+  pod 'Koara'
+
+  target 'KoaraHtml iOS Tests' do
+    pod 'Quick'
+    pod 'Nimble'
+  end
+end
--- Tests/Html5RendererTests.swift
+++ Tests/Html5RendererTests.swift
@@ -3,7 +3,14 @@ import XCTest
 
 class Html5RendererTests: XCTestCase {
     
+    //var document: Document?
+    //var parser: Parser
     var renderer: Html5Renderer?
+    
+    override func setUp() {
+        //parser = new Parser();
+        //renderer = new Html5Renderer();
+    }
     
     func testRenderer() {
         //document = new Parser().parse("Test");
@@ -11,68 +18,39 @@ class Html5RendererTests: XCTestCase {
         //assertEquals("<p>Test</p>", renderer.getOutput());
     }
     
+    func testRenderHardwrapTrue() {
+        //renderer.setHardwrap(true);
+        //document = new Parser().parse("a\nb");
+        //document.accept(renderer);
+        //assertEquals("<p>a<br>\nb</p>", renderer.getOutput());
+    }
     
- 
+    func testRenderPartialFalse() {
+        //StringBuilder expected = new StringBuilder("<!DOCTYPE html>\n");
+        //expected.append("<html>\n");
+        //expected.append("  <body>\n");
+        //expected.append("    <p>Test</p>\n");
+        //expected.append("  </body>\n");
+        //expected.append("</html>\n");
+        
+        //renderer.setPartial(false);
+        //document = new Parser().parse("Test");
+        //document.accept(renderer);
+        //assertEquals(expected.toString(), renderer.getOutput());
+    }
     
-    /*
-        private Document document;
-        private Parser parser;
-        private Html5Renderer renderer;
-        
-        @Before
-        public void setUp() {
-        parser = new Parser();
-        renderer = new Html5Renderer();
-        }
-        
-        @Test
-        public void testRender() {
-        document = new Parser().parse("Test");
-        document.accept(renderer);
-        assertEquals("<p>Test</p>", renderer.getOutput());
-        }
-        
-        @Test
-        public void testRenderHardwrapTrue() {
-        renderer.setHardwrap(true);
-        document = new Parser().parse("a\nb");
-        document.accept(renderer);
-        assertEquals("<p>a<br>\nb</p>", renderer.getOutput());
-        }
-        
-        @Test
-        public void testRenderPartialFalse() {
-        StringBuilder expected = new StringBuilder("<!DOCTYPE html>\n");
-        expected.append("<html>\n");
-        expected.append("  <body>\n");
-        expected.append("    <p>Test</p>\n");
-        expected.append("  </body>\n");
-        expected.append("</html>\n");
-        
-        renderer.setPartial(false);
-        document = new Parser().parse("Test");
-        document.accept(renderer);
-        assertEquals(expected.toString(), renderer.getOutput());
-        }
-        
-        
-        @Test
-        public void testHeadingIdsTrue() {
-        renderer.setHeadingIds(true);
-        document = new Parser().parse("= A");
-        document.accept(renderer);
-        assertEquals("<h1 id=\"a\">A</h1>", renderer.getOutput());
-        }
-        
-        @Test
-        public void testHeadingIdsTrueMultipleWords() {
-        renderer.setHeadingIds(true);
-        document = new Parser().parse("= This is a test");
-        document.accept(renderer);
-        assertEquals("<h1 id=\"this_is_a_test\">This is a test</h1>", renderer.getOutput());
-        }
-        
-        
-    }*/
+    func testHeadingIdsTrue() {
+        //renderer.setHeadingIds(true);
+        //document = new Parser().parse("= A");
+        //document.accept(renderer);
+        //assertEquals("<h1 id=\"a\">A</h1>", renderer.getOutput());
+    }
     
+    func testHeadingIdsTrueMultipleWords() {
+        //renderer.setHeadingIds(true);
+        //document = new Parser().parse("= This is a test");
+        //document.accept(renderer);
+        //assertEquals("<h1 id=\"this_is_a_test\">This is a test</h1>", renderer.getOutput());
+    }
+     
 }
